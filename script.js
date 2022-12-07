df7543c2464f63deaf7d5335b7d0070abb1515bb
anikateagrawal <anikateag7316@gmail.com>
2022-12-07 22:48:42 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add(){
     var tex=document.getElementById("text");
-    var t=tex.ariaValueMax;
+    var t=tex.value;
     var col=document.querySelector("[name=t]:checked");
     var colid=col.id;
     var ticketscon=document.getElementById("tt");
@@ -10,7 +10,10 @@ function add(){
     <div class="tag"></div>
     <div class="cont"></div>`;
     ticketscon.appendChild(ticket);
-
+    console.log(col.id);
+    ticket.children[0].style.backgroundColor=`${col.id.substring(0,col.id.length-1)}`;
+    ticket.children[2].innerText=t;
+    ticket.addEventListener("click",del);
 }
 var ad=document.getElementById("add");
 ad.addEventListener("click",show);
@@ -32,4 +35,13 @@ function key(e){
 
 function hide(){
     document.getElementById("ttt").style.display="none";
+}
+document.re
+
+function del(e){
+    console.log(e.target.parentElement);
+    if(document.getElementById("delet").checked==true)
+    {
+        e.target.parentElement.removeChild(e.target);
+    }
 }
