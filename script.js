192f20d8aa7675797a486a4447635d1f1a1716bd
anikateagrawal <anikateag7316@gmail.com>
2022-12-07 22:27:45 +0530

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(){
+    var tex=document.getElementById("text");
+    var t=tex.ariaValueMax;
+    var col=document.querySelector("[name=t]:checked");
+    var colid=col.id;
+    var ticketscon=document.getElementById("tt");
+    var ticket=document.createElement("div");
+    ticket.className="tick";
+    ticket.innerHTML=`<div class="co"></div>
+    <div class="tag"></div>
+    <div class="cont"></div>`;
+    ticketscon.appendChild(ticket);
+
+    
+}
+var ad=document.getElementById("add");
+ad.addEventListener("click",show);
+
+function show(){
+    document.getElementById("ttt").style.display="flex";
+}
+
+document.addEventListener("keypress",key);
+
+function key(e){
+    console.log(e.key);
+    if(e.key=="Enter")
+    {
+        add();
+        hide();
+    }
+}
+
+function hide(){
+    document.getElementById("ttt").style.display="none";
+}
