9cac5ccb934d6a0fd4195fad0303a76924d6f19e
anikateagrawal <anikateag7316@gmail.com>
2022-12-07 23:04:40 +0530

    commit

--- script.js
+++ script.js
@@ -11,16 +11,41 @@ function add(){
     <div class="cont"></div>`;
     ticketscon.appendChild(ticket);
     console.log(Math.floor(Math.random()*10000));
-    ticket.children[1].innerText="#"+Math.floor(Math.random()*10000);
+    var tag="#"+Math.floor(Math.random()*10000);
+    ticket.children[1].innerText=tag;
     ticket.children[0].style.backgroundColor=`${col.id.substring(0,col.id.length-1)}`;
     ticket.children[2].innerText=t;
     ticket.addEventListener("click",del);
     ticks={
-        id:'dd'
+        id:`${tag}`,
+        col:`${col.id.substring(0,col.id.length-1)}`,
+        con:`${t}`
     }
+    arr.push(ticks);
+    localStorage.setItem('ticks',JSON.stringify(arr));
 }
 var arr=[];
-
+window.onload=dis();
+function dis(){
+    if(localStorage.getItem('ticks')!=null){
+        var a=JSON.parse(localStorage.getItem('ticks'));
+        var ticketscon=document.getElementById("tt");
+        a.forEach(element => {
+    var ticket=document.createElement("div");
+    ticket.className="tick";
+    ticket.innerHTML=`<div class="co"></div>
+    <div class="tag"></div>
+    <div class="cont"></div>`;
+    ticketscon.appendChild(ticket);
+    // console.log(Math.floor(Math.random()*10000));
+    // var tag="#"+Math.floor(Math.random()*10000);
+    ticket.children[1].innerText=element.id;
+    ticket.children[0].style.backgroundColor=`${element.col}`;
+    ticket.children[2].innerText=element.con;
+    ticket.addEventListener("click",del);
+        });
+    }
+}
 var ad=document.getElementById("add");
 ad.addEventListener("click",show);
 
